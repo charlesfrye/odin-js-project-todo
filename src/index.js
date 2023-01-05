f9d7f21da3d5b07852ecd2b689744716fbb20b9f
Charles Frye <charlesfrye@Teddys-MacBook-Pro.local>
2023-01-05 13:07:26 -0500

    updates file modes

--- src/index.js
+++ src/index.js
@@ -3,3 +3,24 @@ import * as projects from "./projects";
 import * as todos from "./todos";
 
 import "./style.css";
+
+class Todo {
+  // constructor
+  // getter/setter for date and description
+  // getter/setter for completion state
+  // getter/setter for data in localStorage
+}
+
+class Project {
+  // constructor
+  // getter/setter for collection of Todos
+  // getter/setter for name
+  // getter/setter for data in localStorage
+  //   factor through todo
+}
+
+class Portfolio {
+  // constructor
+  // get/set data in localStorage?
+  //   yes, but factor through todo/project
+}

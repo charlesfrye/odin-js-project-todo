22aae3b68c41c5b14b7e4c4c07da798b12521b2f
Charles Frye <cfrye59@gmail.com>
2023-01-06 20:07:27 -0700

    changes font to OpenSans

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
 
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: "asset/resource",
+      },
     ],
   },
 };
